f83480a9b32d5491d814c5f81d5ebc463caddb71
Tute <mateoyastor60@gmail.com>
2024-04-02 02:22:01 -0300

    update ui

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,18 +1,42 @@
-"use client"
-import {signOut} from 'next-auth/react'
+"use client";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
 
 function DashboardPage() {
   return (
-    <section className="h-[calc(100vh-7rem)] flex justify-center items-center">
+    <section className="min-h-screen flex justify-center items-center bg-[#111111]">
       <div>
         <h1 className="text-white text-5xl">Dashboard</h1>
-        <button className="bg-white text-black px-4 py-2 rounded-md mt-4"
-          onClick={() => signOut()}
-        >
-          Logout
-        </button>
+        <div className="flex flex-wrap">
+          <Link href="/">
+            <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+              <span className="absolute inset-0 overflow-hidden rounded-full">
+                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+              </span>
+              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                <span>{`Home`}</span>
+              </div>
+              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+            </button>
+          </Link>
+
+          <Link href="/">
+            <button
+              onClick={() => signOut()}
+              className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block"
+            >
+              <span className="absolute inset-0 overflow-hidden rounded-full">
+                <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+              </span>
+              <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                <span>{`Logout`}</span>
+              </div>
+              <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+            </button>
+          </Link>
+        </div>
       </div>
     </section>
-  )
+  );
 }
-export default DashboardPage
+export default DashboardPage;
