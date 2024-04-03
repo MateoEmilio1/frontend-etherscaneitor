7c49156631a61005269a5fa956b42bfbe6d40b66
Tute <mateoyastor60@gmail.com>
2024-04-03 11:16:07 -0300

    show user data

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,17 +1,25 @@
-
-import { signOut } from "next-auth/react";
-import Link from "next/link";
-import CustomButton from "../components/customBotton";
 import ButtonsNavigation from "./buttonsNav";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/libs/db";
 
-export default function DashboardPage() {
+export default  async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+  console.log(session?.user);
+
   return (
     <section className="min-h-screen flex justify-center items-center bg-[#111111]">
       <div>
         <h1 className="text-white text-5xl">Dashboard</h1>
+        <h1 className="text-white text-5xl">Welcome back {session.user.name}</h1>
+        <p className="text-white">
+          User data:<br/>
+          Email: {session.user.email} <br/>
 
-        
+        </p>
+
+
         <ButtonsNavigation />
+        
 
        
       </div>
