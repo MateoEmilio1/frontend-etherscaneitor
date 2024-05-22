3b373485c24287f7e303f529cfa1921923ca2ac4
Tute <mateoyastor60@gmail.com>
2024-05-21 21:13:43 -0300

    feat: pulse data

--- app/components/cardEthGasPrice.js
+++ app/components/cardEthGasPrice.js
@@ -15,8 +15,10 @@ export default function EthGasPrice() {
         //Prod:https://backend-etherneitor-production.up.railway.app/getEthGasPrice
 
         //const response = await axios.post("http://localhost:5001/getEthGasPrice");
-        
-        const response = await axios.post("https://backend-etherneitor-production.up.railway.app/getEthGasPrice");
+
+        const response = await axios.post(
+          "https://backend-etherneitor-production.up.railway.app/getEthGasPrice"
+        );
 
         const gasPriceInWei = parseInt(response.data.result, 16); // Convert hex to decimal
         const gasPriceInGwei = gasPriceInWei / 1e9; // Convert wei to gwei
@@ -33,7 +35,20 @@ export default function EthGasPrice() {
   }, []);
 
   if (loading) {
-    return <div className="text-center">Loading...</div>;
+    return (
+      <div className="max-w-sm mx-auto xl:mt-32 2xl:mt-20 rounded-lg p-6 text-white text-center font-mono">
+        <h2 className=" text-2xl font-semibold mb-4">
+          Ethereum Gas Price
+        </h2>
+
+        <div className="flex flex-wrap text-lg mb-2">
+          <span className="ml-4 font-semibold ">Gas Price:</span>{" "}
+          <div className="mt-3 ml-2 rounded-full animate-pulse bg-blue-200 h-2 w-12">
+            <span className=" h-2 bg-slate-200 rounded col-span-1 text-blue-300"></span>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   if (error) {
--- app/components/cardEthereumTVL.js
+++ app/components/cardEthereumTVL.js
@@ -1,6 +1,6 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CardEthereumTVL() {
   const [tvlData, setTvlData] = useState(null);
@@ -10,13 +10,15 @@ export default function CardEthereumTVL() {
   useEffect(() => {
     const fetchTVLData = async () => {
       try {
-        const response = await axios.get('https://api.llama.fi/v2/chains');
-        const ethData = response.data.find(chain => chain.name === 'Ethereum');
+        const response = await axios.get("https://api.llama.fi/v2/chains");
+        const ethData = response.data.find(
+          (chain) => chain.name === "Ethereum"
+        );
         setTvlData(ethData);
         setLoading(false);
       } catch (err) {
         console.error(err);
-        setError('Error fetching data');
+        setError("Error fetching data");
         setLoading(false);
       }
     };
@@ -25,14 +27,28 @@ export default function CardEthereumTVL() {
   }, []);
 
   const formatCurrency = (value) => {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
     }).format(value);
   };
 
   if (loading) {
-    return <div className="text-center">Loading...</div>;
+    return (
+      <div className="max-w-sm mx-auto rounded-lg p-6">
+        <div className="text-white flex flex-wrap">
+          <h2 className="text-4xl font-semibold text-center mb-4">
+            Ethereum Total Value Locked
+          </h2>
+          <div className="flex flex-wrap text-xl mb-2 text-center ">
+            <span className="ml-10 font-semibold font-mono">TVL:</span>{" "}
+            <div className="mt-2 ml-2 rounded-full animate-pulse bg-blue-200 h-4 w-48">
+              <span className="h-2 bg-slate-200 rounded col-span-1 text-blue-300"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   if (error) {
@@ -42,9 +58,14 @@ export default function CardEthereumTVL() {
   return (
     <div className="max-w-sm mx-auto rounded-lg p-6">
       {tvlData ? (
-        <div className='text-white '>
-          <h2 className="text-4xl font-semibold text-center mb-4">Ethereum Total Value Locked</h2>
-          <p className="text-xl mb-2 text-center "><span className="font-semibold font-mono">TVL:</span> {formatCurrency(tvlData.tvl)}</p>
+        <div className="text-white ">
+          <h2 className="text-4xl font-semibold text-center mb-4">
+            Ethereum Total Value Locked
+          </h2>
+          <p className="text-xl mb-2 text-center ">
+            <span className="font-semibold font-mono">TVL:</span>{" "}
+            {formatCurrency(tvlData.tvl)}
+          </p>
         </div>
       ) : (
         <div className="text-center">No data found for Ethereum</div>
