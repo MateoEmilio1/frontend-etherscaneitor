1ca1707b1dce440ff109bae5cebd1a9a6cba6538
MateoEmilio1 <mateoyastor60@gmail.com>
2025-02-03 18:28:29 -0300

    feat: analytics

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import Script from "next/script";
 
 import Footer from "./components/footer";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 import Navbar from "./components/sessionComp";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +16,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-X8CPGZR5T7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-X8CPGZR5T7');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
-        <Navbar /> {children} <Footer /><Toaster />
+        <Navbar />
+        {children}
+        <Footer />
+        <Toaster />
       </body>
     </html>
   );
